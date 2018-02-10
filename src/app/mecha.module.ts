97f9b585775b0ad5d4c6f1378635bbef929ffc5d
lintmouse <paul.palubinski@gmail.com>
2018-02-10 13:05:43 -0700

    Made options optional

--- src/app/mecha.module.ts
+++ src/app/mecha.module.ts
@@ -4,16 +4,17 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { APP_CONFIG, AppConfigInterface } from './abstract/app-config.abstract';
 import { CACHE, CacheInterface } from './abstract/cache.abstract';
-import { MechaHttpService } from './http/services/mecha-http/mecha-http.service';
 import { MechaCacheService } from './shared/services/mecha-cache/mecha-cache.service';
+import { MechaHttpService } from './http/services/mecha-http/mecha-http.service';
+import { MechaUtilService } from './shared/services/mecha-util/mecha-util.service';
 
 const defaultConfig: AppConfigInterface = {
   cacheTtl: 60000,
 };
 
 export interface MechaModuleOptionsInterface {
-  appConfig: AppConfigInterface;
-  cacheClass: new (...args: any[]) => CacheInterface;
+  appConfig?: AppConfigInterface;
+  cacheClass?: new (...args: any[]) => CacheInterface;
 }
 
 @NgModule({
@@ -23,6 +24,7 @@ export interface MechaModuleOptionsInterface {
   ],
   providers: [
     MechaHttpService,
+    MechaUtilService,
   ],
 })
 export class MechaModule {
